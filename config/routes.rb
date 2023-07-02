2f52a5e7b8c8572c0497dc07ebbc51d2abba47f4
YukiWatanabe824 <gyggg950@gmail.com>
2023-07-02 17:50:34 +0900

    rootの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   resources :maps
   resources :spots
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "home#index"
 end
