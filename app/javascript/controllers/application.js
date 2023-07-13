5eef568ac00516b1e7f4f45c7363dfac98d8b7d7
YukiWatanabe824 <gyggg950@gmail.com>
2023-07-14 06:31:49 +0900

    マップの追加、viewの作成

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,15 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoieXVraXdhdGFuYWJlIiwiYSI6ImNsamx3NmQ2NzEwZTczZXBwOTUzbDE5amsifQ.ep62YrWWsdT4HtVFLVA-Og';
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/yukiwatanabe/cljqcpwss004501oc6qhs4rek', // style URL
+center: [139.81010425, 35.71001875], // starting position [lng, lat]
+zoom: 9, // starting zoom
+});
+
+
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,7 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@mapbox-gl", to: "mapbox-gl.js",preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@2.15.0/dist/mapbox-gl.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
