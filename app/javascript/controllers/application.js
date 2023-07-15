4443e059d845ea720bbafcfb03f0e25085837ea1
YukiWatanabe824 <gyggg950@gmail.com>
2023-07-15 15:31:46 +0900

    マップ上のスポットにPOPUPで詳細表示する機能の追加

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -12,12 +12,6 @@ import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoieXVraXdhdGFuYWJlIiwiYSI6ImNsamx3NmQ2NzEwZTczZXBwOTUzbDE5amsifQ.ep62YrWWsdT4HtVFLVA-Og";
-const map = new mapboxgl.Map({
-  container: "map", // container ID
-  style: "mapbox://styles/yukiwatanabe/cljqcpwss004501oc6qhs4rek", // style URL
-  center: [139.81010425, 35.71001875], // starting position [lng, lat]
-  zoom: 9, // starting zoom
-});
 
 const mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
 mapboxClient.geocoding
@@ -41,19 +35,34 @@ mapboxClient.geocoding
 
     const feature = response.body.features[0];
 
+    // マップを表示
     const map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: feature.center,
-      zoom: 10,
+      container: "map", // container ID
+      style: "mapbox://styles/yukiwatanabe/cljqcpwss004501oc6qhs4rek", // style URL
+      center: feature.center, // starting position [lng, lat]
+      zoom: 14, // starting zoom
     });
 
-    new mapboxgl.Marker().setLngLat(feature.center).addTo(map);
-  });
+    // create geocoder
+    map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+      })
+    );
 
-map.addControl(
-  new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl,
-  })
-);
+    // create the popup
+    const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      "ここが越谷レイクタウン"
+    );
+
+    // create DOM element for the marker
+    const el = document.createElement("div");
+    el.id = "marker";
+
+    // create the marker with popup
+    new mapboxgl.Marker()
+      .setLngLat(feature.center)
+      .setPopup(popup)
+      .addTo(map);
+  });
