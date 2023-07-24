0de9728e43de2d97826ed7550512248a66375aa7
YukiWatanabe824 <gyggg950@gmail.com>
2023-07-24 13:52:43 +0900

    ユーザーテーブルを修正してuidを消去、メールアドレスに一本化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_23_075510) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_045108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,7 +33,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_23_075510) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "uid"
     t.string "password"
     t.string "avatar_url"
     t.datetime "created_at", null: false
