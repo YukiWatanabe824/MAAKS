962854fe17a879a57a59ae7ecd093c0aee425b1b
YukiWatanabe824 <gyggg950@gmail.com>
2023-07-25 11:35:13 +0900

    ユーザーテーブルの構成変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_24_045108) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_25_023132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_24_045108) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "password"
-    t.string "avater_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
