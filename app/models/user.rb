f34aa02704bb5c443b9ff571cbe39f34279962f6
YukiWatanabe824 <gyggg950@gmail.com>
2023-07-26 17:12:56 +0900

    OAuthでのユーザー登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2]
   has_many :spot, dependent: :destroy
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token
+    end
+  end
 end
