c0584c24b82d8cad3e058433995fcd4c506714b2
YukiWatanabe824 <gyggg950@gmail.com>
2023-07-26 17:06:05 +0900

    userモデルにOAuth用のカラムを追加、ユニーク制約/INDEXを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_050811) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_26_073607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,8 +70,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_050811) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "avatar"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
