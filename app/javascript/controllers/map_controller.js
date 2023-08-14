e44f591fdfb8c878852e221258c8d94093f1f035
YukiWatanabe824 <gyggg950@gmail.com>
2023-08-14 11:31:22 +0900

    マーカー表示とスポット追加機能を追加

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,20 +3,40 @@ import mapboxgl from "mapbox-gl";
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  static targets = ["map"];
+  static targets = ["map", "drawer"];
+  static values = {
+    spotid: String,
+  };
 
   connect() {
     mapboxgl.accessToken =
       "pk.eyJ1IjoieXVraXdhdGFuYWJlIiwiYSI6ImNsamx3NmQ2NzEwZTczZXBwOTUzbDE5amsifQ.ep62YrWWsdT4HtVFLVA-Og";
     // マップを表示
-    const map = new mapboxgl.Map({
+    this.mapTarget.mapbox = new mapboxgl.Map({
       container: "map", // container ID
       style: "mapbox://styles/yukiwatanabe/cljqcpwss004501oc6qhs4rek", // style URL
       center: [139.791003, 35.777343], // starting position [lng, lat]
       zoom: 12, // starting zoom
     });
-    this.createGeoCorder(map);
-    this.getSpots(map)
+    this.createGeoCorder(this.mapTarget.mapbox);
+    this.getSpots(this.mapTarget.mapbox);
+  }
+
+  zoomedMap(){
+    this.mapTarget.mapbox.jumpTo({
+      center: [this.mapTarget.newMarker._lngLat.lng,this.mapTarget.newMarker._lngLat.lat],
+      zoom: (this.mapTarget.mapbox.getZoom() + 1)
+    })
+    document.querySelector("#spot_menu").remove()
+  }
+
+  showDrawer() {
+    if (this.drawerTarget.checked){
+    } else {
+      document.querySelector("#longitude").value = this.mapTarget.newMarker._lngLat.lng
+      document.querySelector("#latitude").value = this.mapTarget.newMarker._lngLat.lng
+    }
+    this.drawerTarget.checked = !this.drawerTarget.checked;
   }
 
   getSpots(map) {
@@ -25,22 +45,61 @@ export default class extends Controller {
       .then((spots) => {
         // DBから取得したスポットの描画
         for (const spot of spots) {
-          // create the popup
-          const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-            `<div><h2>${spot.title}</h2><ul><li>${spot.accident_date}</li><li>${spot.accident_type}</li><li>${spot.contents}</li></ul></div>`
-          );
           // create DOM element for the marker
           const el = document.createElement("div");
-          el.id = "marker";
-          el.class = "spot_marker"
+          el.id = spot.id;
+          el.className = "spot_marker color_palette solid_icon";
+
           // create the marker with popup
-          new mapboxgl.Marker()
+          new mapboxgl.Marker({
+            element: el,
+          })
             .setLngLat([spot.longitude, spot.latitude])
-            .setPopup(popup)
             .addTo(map);
         }
       });
+  }
+
+  deleteSpotMenu(event) {
+    if (event.target === document.querySelector(".mapboxgl-canvas")) {
+      const spotMenu = document.querySelector("#spot_menu");
+      if (spotMenu) {
+        spotMenu.remove();
+      }
+    } else {
+      return;
     }
+  }
+
+  getCoordinates() {
+    if (document.querySelector(`#spot_menu`)) {
+      return;
+    } else {
+      this.createMarker();
+    }
+  }
+
+  createMarker() {
+    this.mapTarget.mapbox.on("click", (e) => {
+      if (this.mapTarget.newMarker) {
+        this.mapTarget.newMarker.remove();
+      }
+      this.mapTarget.newMarker = new mapboxgl.Marker({
+        draggable: true,
+        color: "#cf5d40"
+      })
+
+      const el = this.mapTarget.newMarker.getElement()
+      el.id = "new_spot_marker"
+      el.setAttribute("data-controller", "spot");
+      el.setAttribute("data-spot-target", "spot");
+      el.setAttribute("data-action", "contextmenu->spot#showSpotMenu");
+
+      this.mapTarget.newMarker
+      .setLngLat([e.lngLat.lng, e.lngLat.lat])
+      .addTo(this.mapTarget.mapbox);
+    });
+  }
 
   createGeoCorder(map) {
     map.addControl(
@@ -50,28 +109,4 @@ export default class extends Controller {
       })
     );
   }
-
-  //     let newMarker = "";
-  //     map.on("click", (e) => {
-  //       console.log(`座標は ${e.lngLat}`);
-  //       if (newMarker) {
-  //         newMarker.remove();
-  //         newMarker = new mapboxgl.Marker()
-  //           .setLngLat([e.lngLat.lng, e.lngLat.lat])
-  //           .addTo(map);
-  //       } else {
-  //         newMarker = new mapboxgl.Marker()
-  //           .setLngLat([e.lngLat.lng, e.lngLat.lat])
-  //           .addTo(map);
-  //       }
-  //       let formLatitude = document.querySelector("#latitude");
-  //       formLatitude.value = e.lngLat.lat;
-  //       let formLongitude = document.querySelector("#longitude");
-  //       formLongitude.value = e.lngLat.lng;
-  //     });
-  //   })
-  //   .catch((error) => {
-  //     // エラーハンドリング
-  //     console.error(error);
-  //   });
 }
