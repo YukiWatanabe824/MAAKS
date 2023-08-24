7ba32fceb55ef69c968c3359ed4fdfc37c2e0d1a
YukiWatanabe824 <gyggg950@gmail.com>
2023-08-24 15:55:27 +0900

    userのshow→editをTurbo化した

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :set_user, only: %i[show edit update destroy avatar_destroy]
+  before_action :set_user, only: %i[show update destroy avatar_destroy]
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
@@ -9,7 +9,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = User.new
   end
 
-  def edit; end
+  def edit
+    render partial: 'edit_form'
+  end
 
   def update_resource(resource, params)
     return super if params['password'].present?
