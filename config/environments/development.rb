b375ed5290f03094dac5c7ea439733af9aeec8fc
YukiWatanabe824 <gyggg950@gmail.com>
2023-09-29 22:58:36 +0900

    本番環境同様のエラーが開発環境で発生する設定を不要になったのでスイッチ。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
-  config.consider_all_requests_local = false
+  # config.consider_all_requests_local = false
 end
