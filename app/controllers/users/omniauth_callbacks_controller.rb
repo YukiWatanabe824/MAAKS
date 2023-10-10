ba34d731cdf3e37c10a8ad6ae83bcebb9eb434b4
YukiWatanabe824 <gyggg950@gmail.com>
2023-10-10 22:45:13 +0900

    登録済みの場合のFlashメッセージを追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -40,7 +40,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth'].except(:extra)
-      redirect_to new_user_registration_url
+      redirect_to new_user_registration_url, alert: t('view.this_mail_address_used')
     end
   end
 
