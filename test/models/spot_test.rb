61ecb99f25880b4368b582fc5085ddef9081915f
YukiWatanabe824 <gyggg950@gmail.com>
2023-10-16 15:30:18 +0900

    Spotモデルに異常系のテストを追加

--- test/models/spot_test.rb
+++ test/models/spot_test.rb
@@ -5,5 +5,6 @@ require "test_helper"
 class SpotTest < ActiveSupport::TestCase
   test "spot owned by?" do
     assert spots(:spot1).owned_by?(users(:watanabe))
+    assert_not spots(:spot1).owned_by?(users(:test01))
   end
 end
