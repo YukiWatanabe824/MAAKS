8fa6bf4baaf70a37d045910e2690c6b286a3a989
YukiWatanabe824 <gyggg950@gmail.com>
2023-10-16 15:23:28 +0900

    Userモデルのテストを追加

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_27_090158) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "password"
-    t.string "avater_url"
+    t.string "avatar_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
