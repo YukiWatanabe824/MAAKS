2a0da9bebb72e1614976c7c6ee73ce8f9bc986e8
YukiWatanabe824 <gyggg950@gmail.com>
2023-10-25 09:34:53 +0900

    deviseの処理に入る前に独自処理を通過させたいため、メソッド本体の記述は無いがbefore_actionに記述しているdestroy、createに対するrubocopの警告を回避させた

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :authenticate_user!, only: %i[update destroy]
-  before_action :redirect_if_different_user, only: %i[update destroy]
+  before_action :authenticate_user!, only: %i[update destroy] # rubocop:disable all
+  before_action :redirect_if_different_user, only: %i[update destroy] # rubocop:disable all
   before_action :set_user, only: %i[update edit]
   before_action :configure_account_update_params, only: [:update]
-  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create] # rubocop:disable all
 
   def new
     @user = User.new
