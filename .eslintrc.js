b94b5d0c3188d4bbc0587f310007151885229070
YukiWatanabe824 <gyggg950@gmail.com>
2023-10-29 06:22:48 +0900

    CIに向けてESlint,Prettierの設定追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,33 @@
+/*eslint no-undef: 0*/
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-        "prettier"
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  globals: {
+    window: true,
+    fetch: true,
+    FileReader: true,
+    Event: true,
+    FormData: true,
+  },
+  root: true,
+  rules: {
+    "no-unused-vars": [
+      "error",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+      },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  },
+};
