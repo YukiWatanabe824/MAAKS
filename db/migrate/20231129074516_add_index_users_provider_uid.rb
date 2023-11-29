2455dc58e7fcce8554033dd0ff0eb3bc4157b0bf
YukiWatanabe824 <gyggg950@gmail.com>
2023-11-29 16:53:32 +0900

    実態に即してインデックスや複合ユニークキーを設定

---
+++ db/migrate/20231129074516_add_index_users_provider_uid.rb
@@ -0,0 +1,5 @@
+class AddIndexUsersProviderUid < ActiveRecord::Migration[7.1]
+  def change
+    add_index :users, [:provider, :uid], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_213747) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_074516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,12 +57,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_213747) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "password"
-    t.string "avatar_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -70,10 +70,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_213747) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "avatar"
     t.string "provider"
     t.string "uid"
     t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
