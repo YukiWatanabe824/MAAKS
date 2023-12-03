34e8ff0fbbe2a17f6bf0bf4a6cfa03e17a43ce99
YukiWatanabe824 <gyggg950@gmail.com>
2023-12-04 01:25:54 +0900

    flakyなテスト対応

--- test/system/admin_user_test.rb
+++ test/system/admin_user_test.rb
@@ -62,7 +62,7 @@ class AdminUserTest < ApplicationSystemTestCase
     assert_selector "img[src*='user_icon.webp']"
     visit "/admin/users/#{user.id}/edit"
     page.accept_confirm do
-      find('button.link', text: '画像を削除').click
+      find('#avatar_del_link', text: '画像を削除').click
     end
     assert_selector('#flash', text: '削除しました')
     assert_no_selector "img[src*='user_icon.webp']"
