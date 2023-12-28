71ab5c7ac37963f1f7adc118e6fe174be51919b3
YukiWatanabe824 <gyggg950@gmail.com>
2023-12-28 23:23:28 +0900

    new_session_pathを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :registerable, :timeoutable, :recoverable, :rememberable, :omniauthable, omniauth_providers: %i[google_oauth2]
+  devise :database_authenticatable, :registerable, :timeoutable, :recoverable, :rememberable, :omniauthable, omniauth_providers: %i[google_oauth2]
   has_many :spots, dependent: :destroy
   has_one_attached :avatar, dependent: :destroy do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
