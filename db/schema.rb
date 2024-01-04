bdafbdd6547222d7bc5034b8e2a3526000b8ac30
YukiWatanabe824 <gyggg950@gmail.com>
2024-01-04 10:53:41 +0900

    spotsテーブルにアドレスカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_074516) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_04_015058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_074516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.string "address"
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
