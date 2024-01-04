01bb95ff63f45d58a464d46cbd7fe3624c7f7965
YukiWatanabe824 <gyggg950@gmail.com>
2024-01-04 11:04:29 +0900

    Turboリクエストに介入するメソッドを設置

--- app/javascript/controllers/side_menu_for_register_new_spot_controller.js
+++ app/javascript/controllers/side_menu_for_register_new_spot_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
   connect() {
     this.removeMenu();
     this.fillCoordinatesForField();
+    this.fillAddressForField();
   }
 
   removeMenu() {
@@ -17,5 +18,15 @@ export default class extends Controller {
   fillCoordinatesForField() {
     document.querySelector("#spot_longitude").value = this.mapOutlet.element.newMarker._lngLat.lng;
     document.querySelector("#spot_latitude").value = this.mapOutlet.element.newMarker._lngLat.lat;
+  }
+
+  fillAddressForField(){
+    document.addEventListener("turbo:before-fetch-request", async (event) => {
+      event.preventDefault()
+      console.log("通過チェック")
+
+      event.detail.resume()
+
+    })
   }
 }
