88ddd4a11b745da092dc828be3fa591900e06523
YukiWatanabe824 <gyggg950@gmail.com>
2024-03-26 10:07:11 +0900

    スポット登録ウインドウを分離させ、スライドインの動作を作成

--- app/javascript/controllers/new_spot_marker_controller.js
+++ app/javascript/controllers/new_spot_marker_controller.js
@@ -4,5 +4,11 @@ export default class extends Controller {
   connect() {
     const url = "spots/new";
     Turbo.visit(url, { frame: "side_menu" }); //eslint-disable-line
+    this.displayForRegWindow()
+  }
+
+  displayForRegWindow(){
+    const el = document.querySelector(".aside_for_reg")
+    el.classList.add("display")
   }
 }
