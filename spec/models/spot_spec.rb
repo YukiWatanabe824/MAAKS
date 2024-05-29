86589ed56417af20efbf04d601bcc61d059aa95f
YukiWatanabe824 <gyggg950@gmail.com>
2024-05-29 13:38:50 +0900

    リファクタ

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -3,8 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
-  it 'userがspotをもっていることが.own_byでわかる' do
-    # スポットを持つuserを作る
+  it 'spotの所有者が.owned_byでわかる' do
     user_owned_spot = User.new(name: '', email: '', encrypted_password: '')
     spot = user_owned_spot.spots.new(title: '西新井での単独落車事故',
                                      accident_type: '自損',
@@ -14,16 +13,8 @@ RSpec.describe Spot, type: :model do
                                      latitude: 35.684420,
                                      created_at: '2014-01-01 00:00:01',
                                      updated_at: '2014-01-01 00:00:01')
-    # spot = user_owned_spot.spots.first
-    # スポットを持たないユーザーを作る
     user_not_owned_spot = User.new(name: '', email: '', encrypted_password: '')
-    # 初期段階はPOROをつかう
-    # 第二段階でfactoryでuserとspotを作る
-
-    # spotを持つuserが.own_byでspotをもっていることをテストする
     expect(spot.owned_by?(user_owned_spot)).to be_truthy
     expect(spot.owned_by?(user_not_owned_spot)).to be_falsey
-
-    # spotを持たないuserが.own_byでspotを持っていないことをテストする
   end
 end
