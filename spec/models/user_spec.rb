843ea50902bd18c26ab4b404c3013d3ddbe6ca25
YukiWatanabe824 <gyggg950@gmail.com>
2024-05-30 08:41:52 +0900

    user specの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "adminかどうか" do
+  it 'adminかどうか' do
+    admin_user = User.new(name: '', email: '', encrypted_password: '', admin: true)
+    not_admin_user = User.new(name: '', email: '', encrypted_password: '')
 
+    expect(admin_user.admin?).to be_truthy
+    expect(not_admin_user.admin?).to be_falsey
   end
 
-  it "36桁のユニークIDが生成できているかどうか" do
-  
+  it '36桁のユニークIDが生成できているかどうか' do
+    expect(User.create_unique_string.length).to eq 36
   end
 end
