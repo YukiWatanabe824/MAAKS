3cd83436caf1e7b87985366ab18c0f710bf04931
YukiWatanabe824 <gyggg950@gmail.com>
2024-06-02 18:14:33 +0900

    GemfileでFacotry_botの自動読み込みを停止するため、helperで明示的に読み込むように修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe User, type: :model do
   it 'adminかどうか' do
     admin_user = FactoryBot.create(:admin_user)
     not_admin_user = FactoryBot.create(:not_admin_user)
-
     expect(admin_user.admin?).to be_truthy
     expect(not_admin_user.admin?).to be_falsey
   end
