4caffcd053164e46eb0e826fd85335620f3316fe
YukiWatanabe824 <gyggg950@gmail.com>
2024-06-04 05:50:11 +0900

    testの修正

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -5,8 +5,8 @@ require 'rails_helper'
 RSpec.describe Spot, type: :model do
   it 'spotの所有者が.owned_byでわかる' do
     spot = FactoryBot.create(:spot, :make_by_admin_user)
-    user_not_owned_spot = FactoryBot.create(:spot, :make_by_standard_user)
-    expect(spot.owned_by?(user_owned_spot)).to be_truthy
+    user_not_owned_spot = FactoryBot.create(:user, :not_admin_user)
+    expect(spot.owned_by?(spot.user)).to be_truthy
     expect(spot.owned_by?(user_not_owned_spot)).to be_falsey
   end
 end
