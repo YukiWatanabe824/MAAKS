189a1b19675191f5286e2803d88d6d957df2b427
YukiWatanabe824 <gyggg950@gmail.com>
2024-06-04 05:48:32 +0900

    spotのモデルSpecを追加。アソシエーションを利用

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
     sequence(:email) { |n| "xxx#{n}@example.jp" }
     sequence(:password) { |n| "123456789ab#{n}" }
 
-    factory :admin_user do
+    trait :admin_user do
       name { 'yuki' }
       admin { 'true' }
     end
 
-    factory :not_admin_user do
+    trait :not_admin_user do
       name { 'not_admin' }
     end
   end
--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -4,16 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
   it 'spotの所有者が.owned_byでわかる' do
-    user_owned_spot = User.new(name: '', email: '', encrypted_password: '')
-    spot = user_owned_spot.spots.new(title: '西新井での単独落車事故',
-                                     accident_type: '自損',
-                                     contents: '走行中に左折レーンに入ってしまい直進しようとした際に、後方車列を振り向いて確認した際にふらついて落車',
-                                     accident_date: '2014-01-01 00:00:01',
-                                     longitude: 139.774373,
-                                     latitude: 35.684420,
-                                     created_at: '2014-01-01 00:00:01',
-                                     updated_at: '2014-01-01 00:00:01')
-    user_not_owned_spot = User.new(name: '', email: '', encrypted_password: '')
+    spot = FactoryBot.create(:spot, :make_by_admin_user)
+    user_not_owned_spot = FactoryBot.create(:spot, :make_by_standard_user)
     expect(spot.owned_by?(user_owned_spot)).to be_truthy
     expect(spot.owned_by?(user_not_owned_spot)).to be_falsey
   end
