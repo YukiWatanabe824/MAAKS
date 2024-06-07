428604d4bcbd29f1a12ac99c73b8268dc7186c9c
YukiWatanabe824 <gyggg950@gmail.com>
2024-06-08 06:40:53 +0900

    リクエストスペック実装〜リファクタ

--- spec/requests/admin_user_authorizations_spec.rb
+++ spec/requests/admin_user_authorizations_spec.rb
@@ -3,12 +3,46 @@
 require 'rails_helper'
 
 RSpec.describe 'AdminUserAuthorizations', type: :request do
-  describe 'GET edit_admins_user_path' do
-    it 'regular user is denied access' do
-      user = FactoryBot.create(:user, :not_admin_user)
-      sign_in user
+  let(:regular_user) { FactoryBot.create(:user, :not_admin_user) }
+  let(:other_user) { FactoryBot.create(:user, :admin_user) }
+  let(:spot) { FactoryBot.create(:spot, :make_by_standard_user) }
 
-      get edit_admins_user_path(user)
+  describe 'GET edit_admins_user_path' do
+    it 'regular user cant access to editer for admin' do
+      sign_in regular_user
+      get edit_admins_user_path(regular_user)
+      expect(response).to have_http_status '404'
+    end
+  end
+
+  describe 'DELETE admins_user_path' do
+    it 'regular user cant delete other user' do
+      sign_in regular_user
+      delete admins_user_path(other_user)
+      expect(response).to have_http_status '404'
+    end
+  end
+
+  describe 'PUT admins_user_path' do
+    it 'regular user cant edit myself by admins path' do
+      sign_in regular_user
+      put admins_user_path(regular_user)
+      expect(response).to have_http_status '404'
+    end
+  end
+
+  describe 'PUT admins_spot_path' do
+    it 'regular user cant edit myspot by admins path' do
+      sign_in regular_user
+      put admins_spot_path(spot)
+      expect(response).to have_http_status '404'
+    end
+  end
+
+  describe 'DELETE admins_spot_path' do
+    it 'regular user cant delete myspot by admins pass' do
+      sign_in regular_user
+      delete admins_spot_path(spot)
       expect(response).to have_http_status '404'
     end
   end
