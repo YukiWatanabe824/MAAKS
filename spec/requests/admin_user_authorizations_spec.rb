fab026c5c1443d1af3caeb143f69e19aaef07523
YukiWatanabe824 <gyggg950@gmail.com>
2024-06-07 13:47:10 +0900

    1つ目のリクエストスペックを仮実装

--- spec/requests/admin_user_authorizations_spec.rb
+++ spec/requests/admin_user_authorizations_spec.rb
@@ -1,10 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "AdminUserAuthorizations", type: :request do
-  describe "GET /admin_user_authorizations" do
-    it "works! (now write some real specs)" do
-      get admin_user_authorizations_path
-      expect(response).to have_http_status(200)
+RSpec.describe 'AdminUserAuthorizations', type: :request do
+  describe 'GET edit_admins_user_path' do
+    it 'regular user is denied access' do
+      user = FactoryBot.create(:user, :not_admin_user)
+      sign_in user
+
+      get edit_admins_user_path(user)
+      expect(response).to have_http_status '404'
     end
   end
 end
