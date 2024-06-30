cc3ec1c81475dcb9729d64feded1daed25ac4c89
YukiWatanabe824 <gyggg950@gmail.com>
2024-06-30 18:38:35 +0900

    Rubocopによる修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it 'adminかどうか' do
     admin_user = FactoryBot.create(:user, :admin_user)
-    not_admin_user = FactoryBot.create(:user, :not_admin_user)
-    expect(admin_user.admin?).to be_truthy
-    expect(not_admin_user.admin?).to be_falsey
+    expect(admin_user).to be_admin
   end
 
   it '36桁のユニークIDが生成できているかどうか' do
-    expect(User.create_unique_string.length).to eq 36
+    expect(described_class.create_unique_string.length).to eq 36
   end
 end
